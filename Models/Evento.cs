c6e8d12aa502c1e31354055610720e041a8c2bed
jasonsch15@gmail.ccom <jasonsch15@gmail.com>
2026-04-15 14:35:01 -0600

    cambios

--- Models/Evento.cs
+++ Models/Evento.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GestionFerias_CTPINVU.Models;
 
@@ -9,18 +10,24 @@ public partial class Evento
 
     public string CodigoEvento { get; set; } = null!;
 
+    [Required(ErrorMessage = "Debe de seleccionar un Centro Educativo")]
     public long CentroId { get; set; }
 
+    [Required(ErrorMessage = "Debe de ingresar el Nombre del Evento")]
     public string NombreEvento { get; set; } = null!;
 
+    [Required(ErrorMessage = "Debe de especificar el Tipo de Feria")]
     public string TipoFeria { get; set; } = null!;
 
     public string? Descripcion { get; set; }
 
+    [Required(ErrorMessage = "Debe de seleccionar la Fecha de Inicio")]
     public DateOnly FechaInicio { get; set; }
 
+    [Required(ErrorMessage = "Debe de seleccionar la Fecha de Fin")]
     public DateOnly FechaFin { get; set; }
 
+    [Required(ErrorMessage = "El Estado del Evento es obligatorio")]
     public string EstadoEvento { get; set; } = null!;
 
     public long? UsuarioCreacion { get; set; }
